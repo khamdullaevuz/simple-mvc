bfb5c3991094450694f9f3fdc5d164981086491c
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-05-10 10:37:24 +0500

    adding controller core functions

--- framework/Controller.php
+++ framework/Controller.php
@@ -4,5 +4,9 @@ namespace Framework;
 
 class Controller
 {
-
+    public function view($name, $args = []): void
+    {
+        extract($args);
+        require __DIR__ . '/../views/'. $name . '.view.php';
+    }
 }
